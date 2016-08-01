c89c0142b801df8f3efc8b6dbdbbdadfb8876899
UtkarshBhimte <utkarshbhimte95@gmail.com>
2016-08-01 23:38:51 +0530

    commit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
 $(document).ready(function(){
 
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+        || location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+           if (target.length) {
+             $('html,body').animate({
+                 scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
+    }
+});
+
   $('#LoginModal, #RegisterModal').on('shown.bs.modal', function () {
     console.log("yes");
     $('#myInput').focus()
   })
+
+
+
 
 });
